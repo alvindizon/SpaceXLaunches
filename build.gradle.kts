2df873aec9df54a5f03658cddf92455d27a9912d
Alvin Dizon <alvin.dizon91@gmail.com>
2023-09-17 12:57:44 +0800

    Add dependencies
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.android.library").version("8.1.0").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("plugin.serialization").version("1.8.21").apply(false)
+    id("app.cash.sqldelight").version("2.0.0").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization")
+    id("app.cash.sqldelight")
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -25,10 +27,33 @@ kotlin {
         }
     }
 
+    val coroutinesVersion = "1.7.3"
+    val ktorVersion = "2.3.3"
+    val sqlDelightVersion = "2.0.0"
+    val dateTimeVersion = "0.4.1"
+
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("app.cash.sqldelight:runtime:$sqlDelightVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("app.cash.sqldelight:android-driver:$sqlDelightVersion")
+            }
+        }
+        val iosMain by getting {
+            // ...
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+                implementation("app.cash.sqldelight:native-driver:$sqlDelightVersion")
             }
         }
         val commonTest by getting {
